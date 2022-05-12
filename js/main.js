ed1365fc5aac4e1167d9e29ffd9f263d37a189f9
Erisa Sala <erisasala7@gmail.com>
2022-05-12 17:01:42 +0200

    test

---
+++ js/main.js
@@ -0,0 +1,90 @@
+const authentification = firebase.auth();
+const message = firebase.messaging();
+
+authentification.onAuthStateChanged(handleAuthentificationEvent);
+
+function signInFunction() {
+    authentification.signInWithPopup(new firebase.auth.GoogleAuthProvider());
+}
+
+function signOutFunction() {
+    authentification.signOut();
+}
+
+function handleAuthentificationEvent(usr) {
+    if (usr) {
+        console.log(usr);
+        document.getElementById('sign_in_btn').setAttribute("hidden", "true");
+        document.getElementById('sign_out_btn').removeAttribute("hidden");
+    } else {
+        console.log(usr);
+        document.getElementById('sign_out_btn').setAttribute("hidden", "true");
+        document.getElementById('sign_in_btn').removeAttribute("hidden");
+    }
+}
+
+var tokenList = [{
+    token: "token",
+    uid: "authentification.currentUser.uid"
+}];
+
+function subscribeFunction() {
+    message.requestPermission()
+        .then(() => message.getToken())
+        .then((token) => {
+            console.log(token)
+            alert(token);
+            tokenList.push({
+                token: token,
+                uid: authentification.currentUser.uid
+            });
+            console.log(tokenList)
+        })
+        .catch((e) => console.log(e));
+    document.getElementById('sub_btn').setAttribute("hidden", "true");
+    document.getElementById('unsub_btn').removeAttribute("hidden");
+}
+
+function unsubscribeFunction() {
+    message.getToken()
+        .then((token) => {
+
+            var index = tokenList.indexOf(token);
+            tokenList.splice(index, 1);
+            console.log(tokenList)
+        })
+        .catch((e) => console.log(e));
+    document.getElementById('unsub_btn').setAttribute("hidden", "true");
+    document.getElementById('sub_btn').removeAttribute("hidden");
+}
+var notificaions = [];
+
+function sendNotification(e) {
+    e.preventDefault();
+
+    //const notificationMessage = document.getElementById('notification-message').value;
+    //if ( !notificationMessage ) return;
+    var myArray = [
+        "FM1",
+        "FM2",
+        "FM3",
+    ];
+
+    var randomItem = myArray[Math.floor(Math.random() * myArray.length)];
+    var date = today.getFullYear() + '-' + (today.getMonth() + 1) + '-' + today.getDate();
+    var time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds() + ":" + today.getMilliseconds();
+
+    notificaions
+        .push({
+            fehlermeldung: randomItem,
+            date: dateTime,
+            time: time
+        })
+        .then(() => {
+            //  document.getElementById('notification-message').value = "";
+            console.log(notificaions);
+        })
+        .catch((error) => {
+            console.log(error)
+        });
+}
