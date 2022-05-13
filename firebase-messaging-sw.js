b539038b8aea7f04367eb875a7a445ef48c2cddd
Erisa Sala <erisasala7@gmail.com>
2022-05-13 09:48:19 +0200

    test

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -1,18 +1,16 @@
-importScripts('https://www.gstatic.com/firebase/3.8.0/firebase-app.js');
-importScripts('https://www.gstatic.com/firebase/3.8.0/firebase-messaging.js');
-importScripts('https://www.gstatic.com/firebasejs/7.14.6/firebase-app.js');
-importScripts('https://www.gstatic.com/firebasejs/7.14.6/firebase-messaging.js');
+importScripts('https://erisasala7.github.io/firebase-demo/__/firebase/3.8.0/firebase-app.j');
+importScripts('https://erisasala7.github.io/firebase-demo/__/firebase/3.8.0/firebase-messaging.js');
 
-importScripts('https://www.gstatic.com/firebasejs/init.js');
+importScripts('https://erisasala7.github.io/firebase-demo/__/firebase/3.8.0/init.js');
 const firebaseConfig = {
-  apiKey: "AIzaSyCz1be-gMF4fBAFV4fTU0Aqw3FYdyN23Ks",
-  authDomain: "fir-demo-25791.firebaseapp.com",
-  databaseURL: "https://fir-demo-25791-default-rtdb.europe-west1.firebasedatabase.app",
-  projectId: "fir-demo-25791",
-  storageBucket: "fir-demo-25791.appspot.com",
-  messagingSenderId: "616125449750",
-  appId: "1:616125449750:web:a31071734738c5c41b1737",
-  measurementId: "G-F0Y9NMD4G1"
+    apiKey: "AIzaSyCz1be-gMF4fBAFV4fTU0Aqw3FYdyN23Ks",
+    authDomain: "fir-demo-25791.firebaseapp.com",
+    databaseURL: "https://fir-demo-25791-default-rtdb.europe-west1.firebasedatabase.app",
+    projectId: "fir-demo-25791",
+    storageBucket: "fir-demo-25791.appspot.com",
+    messagingSenderId: "616125449750",
+    appId: "1:616125449750:web:a31071734738c5c41b1737",
+    measurementId: "G-F0Y9NMD4G1"
 };
 
 firebase.messaging();
