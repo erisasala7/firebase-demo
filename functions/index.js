90a2b9c46468c2514c00e3ac50445ca7632f7f2e
Erisa Sala <erisasala7@gmail.com>
2022-10-30 14:50:56 +0100

    firebaseApp

--- functions/index.js
+++ functions/index.js
@@ -1,7 +1,26 @@
-const functions = require("firebase-functions");
-const admin = require('firebase-admin');
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
-admin.initializeApp(functions.config().firebase);
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyDPLzT0hG5fnw01ihWfp5tw2zYTrVsab7w",
+    authDomain: "pwawithfirebase.firebaseapp.com",
+    databaseURL: "https://pwawithfirebase-default-rtdb.europe-west1.firebasedatabase.app",
+    projectId: "pwawithfirebase",
+    storageBucket: "pwawithfirebase.appspot.com",
+    messagingSenderId: "591184070902",
+    appId: "1:591184070902:web:86ae79d1599efa996fc4d7"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+// const functions = require("firebase-functions");
+// const admin = require('firebase-admin');
+
+// admin.initializeApp(functions.config().firebase);
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
 //
